dd143d9e43759db6da9338f5595f090041e268ba
dat123jqk <dattruong1209@gmail.com>
2022-07-03 11:03:55 +0700

    code

---
+++ base.js
@@ -0,0 +1,115 @@
+// Những việc cần làm để tạo nên giao diện mucsic
+/*
+1. Render songs (kết xuất bài hát) 
+2. Scroll top
+3. play / pause / seek
+4. CD rotate
+5. Next / prev
+6. Random
+7. Next / repeat when ended
+8. Active song
+9. Scroll active song into view
+10. play song when click
+*/
+
+
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+
+// const app = {
+//     songs: [
+//         {
+//             name: 'why not me',
+//             singer: 'EnriqueIglesias',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\WhyNotMe-EnriqueIglesias-3479372.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\why-not-me.jpg'
+//         },
+    
+//         {
+//             name: 'what makes you beautiful',
+//             singer: 'OneDirection',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\WhatMakesYouBeautiful_OneDirection_46wq.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\what makes you beautiful.jpg'
+//         },
+    
+//         {
+//             name: 'Love Me Like You Do',
+//             singer: 'EllieGoulding',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\LoveMeLikeYouDo-EllieGoulding-4233682.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\love me like you.png'
+//         },
+    
+//         {
+//             name: 'Jar of hearts',
+//             singer: 'ChristinaPerri',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\JarofHearts-ChristinaPerri-5009422.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\jar of hearts.jpg'
+//         },
+    
+//         {
+//             name: 'I Lay My Love On You',
+//             singer: 'Westlife',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\ILayMyLoveOnYou-Westlife-460669.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\i lay my love.jpg'
+//         },
+    
+//         {
+//             name: 'Havana',
+//             singer: 'CamilaCabelloYoungThug',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\Havana-CamilaCabelloYoungThug-5817730.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\havana.jpg'
+//         },
+    
+//         {
+//             name: 'why not me',
+//             singer: 'EnriqueIglesias',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\Faded-AlanWalker-5919763.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\faded.jpg'
+//         },
+    
+//         {
+//             name: 'Despacito',
+//             singer: 'LuisFonsi',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\DespacitoRemix-LuisFonsiDaddyYankeeJustinBieber-5443166.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\despacito.jpg'
+//         },
+    
+//         {
+//             name: 'Build A Bitch',
+//             singer: 'BellaPoarch',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\BuildABitch-BellaPoarch-7030913.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\biuld a bicth.jpg'
+//         },
+    
+//         {
+//             name: 'Cheap Thrills',
+//             singer: 'Sia',
+//             path:'C:\Users\Admin\Quoc Dat Mucsic\accset\mucsic\CheapThrills-Sia-5612594.mp3',
+//             image:'C:\Users\Admin\Quoc Dat Mucsic\accset\img\Cheap Thrills.jpg'
+//         }
+//     ],
+//     render: function(){
+//         // const htmls = this.songs.map((song)=>{
+//         //     return`
+//         //     <div class="song">
+//         //         <div class="thumb" style="background-image: url('${song.image}')">
+//         //         </div>
+//         //         <div class="body">
+//         //             <h3 class="title">${song.name}</h3>
+//         //             <p class="author">${song.singer}</p>
+//         //         </div>
+//         //         <div class="option">
+//         //             <i class="fas fa-ellipsis-h"></i>
+//         //         </div>
+//         //   </div>
+//         //     `
+//         // })
+//         // $('.playlist').innerHTML = htmls.join('')
+//         console.log(123)
+//     },
+//     start: function(){
+//         this.render()
+//     }
+// }
+// app.start()
+
